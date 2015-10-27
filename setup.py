d2e85c5e4bda9db03e306929e6b5e8d38924589e
Alex Crowe <alex@pancentric.com>
2015-10-27 16:27:34 +0000

    made changes for packaging for pypi

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-imgix',
     version='0.1',
-    packages=['django_imgix'],
+    packages=find_packages(),
     include_package_data=True,
     license='BSD License',  # example license
     description='Django app to generate imgix urls in your templates.',
